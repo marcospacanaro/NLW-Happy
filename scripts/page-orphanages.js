b80fb657735b70b29d036ce8640ede636802b44f
Marcos <m.pacanaro@gmail.com>
2020-10-16 20:14:38 -0300

    first commit: add and style pages

---
+++ scripts/page-orphanages.js
@@ -0,0 +1,20 @@
+const mymap = L.map('mapid').setView([-27.222633, -49.6455874], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
+
+const icon = L.icon({
+    iconUrl: "./images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./images/arrow-white.svg"> </a>')
+
+L.marker([-27.222633, -49.6455874], {icon: icon}).addTo(mymap)
+    .bindPopup(popup)
